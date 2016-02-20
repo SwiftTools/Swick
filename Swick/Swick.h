eb6d5f9ce388d882f418bbfd90b35e446fa7901b
Artyom Y. Razinov <artyom.razinov@gmail.com>
2016-02-21 01:16:24 +0300

    Initial Commit

---
+++ Swick/Swick.h
@@ -0,0 +1,19 @@
+//
+//  Swick.h
+//  Swick
+//
+//  Created by Artyom Y. Razinov on 2/21/16.
+//  Copyright © 2016 Artyom Y. Razinov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swick.
+FOUNDATION_EXPORT double SwickVersionNumber;
+
+//! Project version string for Swick.
+FOUNDATION_EXPORT const unsigned char SwickVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swick/PublicHeader.h>
+
+
